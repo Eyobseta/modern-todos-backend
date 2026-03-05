27ad1b58ba7151f2b2a2d70fec7e2dffd9fc1ba5
Eyob Seta <eyobseta50@gmail.com>
2026-03-05 15:59:21 +0300

    todo backend  cors modified and .env modified

--- config/db.js
+++ config/db.js
@@ -1,13 +1,14 @@
-import {Pool} from 'pg';
+import { Pool } from 'pg';
 import { config } from 'dotenv';
 config();
 
 const pool = new Pool({
-    user:process.env.DB_USER,
-    host:process.env.DB_HOST,
-    database:process.env.DB_NAME,
-    password:process.env.DB_PASSWORD,
-    port:process.env.DB_PORT
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
 
 export default pool;
--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: process.env.ORIGIN
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173'
 }));
 
 app.use('/api/todos', todosRouter);
