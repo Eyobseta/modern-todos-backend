64cf26dda2b3990e3cde63eee42945097f886884
Eyob Seta <eyobseta50@gmail.com>
2026-03-05 17:58:39 +0300

    Use DATABASE_URL with family=4

--- config/db.js
+++ config/db.js
@@ -3,13 +3,8 @@ import { config } from 'dotenv';
 config();
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
     ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
-    family: 4 
 });
 
 export default pool;
