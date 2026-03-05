4451aee2e5754984ad05d3b8ce0e61027e00ecad
Eyob Seta <eyobseta50@gmail.com>
2026-03-05 17:47:29 +0300

    todo backend  cors modified and .env modified

--- config/db.js
+++ config/db.js
@@ -8,7 +8,8 @@ const pool = new Pool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
-    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+    family: 4 
 });
 
 export default pool;
